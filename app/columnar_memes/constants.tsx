d1119288688358d0e9a92a09276e272121986395
vlad <vlad@DESKTOP-245OM12>
2025-04-17 13:03:33 +0300

    memasik guide

---
+++ app/columnar_memes/constants.tsx
@@ -0,0 +1,14 @@
+export const memesColumns = [
+  { name: 'NAME', uid: 'name' },
+  { name: 'LINK', uid: 'link' },
+  { name: 'LIKES', uid: 'likes' },
+  { name: 'ACTIONS', uid: 'actions' },
+];
+
+export const labels = {
+  emptyMes: 'The Table is empty',
+  newMemBtn: 'Create New Mem',
+  title: 'Columnar Memes',
+  editBtn: 'Edit',
+  removeBtn: 'Remove',
+};
---
+++ app/linear_memes/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
+import { title } from '@/components/primitives';
+import { MainCard } from '@/components/card';
+import { IMemDataDto } from '@/types/MemDataDto';
+import { getSortedMemesData } from '@/lib/http';
+import { editMem } from '@/lib/http';
+import { labels } from './constants';
+
+export default function linearPage() {
+  const [memes, setMemes] = useState<IMemDataDto[]>([]);
+  const [like, setLike] = useState<boolean>(false);
+  const [curMem, setCurMem] = useState<IMemDataDto | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getSortedMemesData();
+        setMemes(data);
+      } catch (err: any) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    if (like) {
+      (async () => {
+        try {
+          await editMem({
+            _id: curMem?._id || '',
+            name: curMem?.name || '',
+            date: curMem?.date || '',
+            link: curMem?.link || '',
+            likes: (curMem?.likes || 0) + 1,
+          });
+          const data = await getSortedMemesData();
+          setMemes(data);
+        } catch (err: any) {
+          console.log(err);
+        } finally {
+          setLike(false);
+          setCurMem(null);
+        }
+      })();
+    }
+  }, [like]);
+
+  return (
+    <div className="mb-5">
+      <h1 className={title()}>{labels.title}</h1>
+      <div className="flex flex-wrap mt-5">
+        {memes.map(mem => {
+          return (
+            <MainCard
+              item={mem}
+              key={mem?._id}
+              setCurMem={setCurMem}
+              setLike={setLike}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
